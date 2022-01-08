13e804ed76b27c304ccbad176742feff5ddf2431
Smrutiranjan Patra <76831153+Smrutiranjan-Patra@users.noreply.github.com>
2022-01-08 18:21:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+
+import { Animation } from "./compoents/animation";
+import { Header } from "./compoents/header";
+import { Home } from "./compoents/home";
+import { About } from "./compoents/about.jsx";
+import { Project } from "./compoents/project";
+import { Resume } from "./compoents/resume.jsx";
+import { ContactUs } from "./compoents/contact";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">
+    <Animation />
+    <br id="bar1" />
+    <Header />
+    <br id="barhead" />
+    <Home />
+    <br id="bar2" />
+    <About />
+    <br id="bar3" />
+    <Project />
+    <br id="bar4" />
+    <Resume />
+    <br id="bar5" />
+    <ContactUs />
+  </div>
 }
+
+
 
 export default App;
