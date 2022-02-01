97dc654dbf2ea887718546afa4e50271d3a6c479
Smrutiranjan Patra <76831153+Smrutiranjan-Patra@users.noreply.github.com>
2022-02-01 12:47:07 +0530

    update

--- src/compoents/project.jsx
+++ src/compoents/project.jsx
@@ -60,6 +60,32 @@ const Project = () => {
             </button>
           </div>
         </div>
+        <div className="project-box">
+          <div className="img-box">
+            <a href="https://shopper-stop-clone.herokuapp.com/" target="_blank">
+              <img
+                src="https://camo.githubusercontent.com/fa9d45e9b56a4643453d8b9d0930031de89bdfcfb3b12e29cff5950493ce81d5/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f6d61782f3730302f312a344a4545544c7651726e3457636833645179646969412e706e673f7261773d74727565"
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="details">
+            <h3 className="project-name">Quora</h3>
+            <p className="project-details">
+              Quora a platform to ask questions and connect with people who contribute unique insights and quality answers.
+            </p>
+            <p className="tech">
+              React | Redux | | CSS | Chakra UI | Node js | Express js | MongoDB
+            </p>
+
+            <button className="github-btn">
+              <a
+                href="https://github.com/Smrutiranjan-Patra/quora" target="_blank" >
+                Github
+              </a>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
