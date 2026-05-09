a261ff3d5245c538a7135ff5fe37b490ffa6d7e4
Smrutiranjan Patra <guessme.smruti@gmail.com>
2026-05-09 15:41:16 +0530

    feat: add new images and update portfolio.js to streamline asset imports and enhance technology icons

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,26 +1,6 @@
-import jsVisualizerPreview from "../components/images/jsVisualizerPreview.png";
-import html from "../components/images/html-5.png";
-import css from "../components/images/css3.png";
-import js from "../components/images/javascript.png";
-import node from "../components/images/node-js.png";
-import express from "../components/images/Express.png";
-import npm from "../components/images/npm.png";
-import mongo from "../components/images/MongoDB.png";
-import psql from "../components/images/PostgresSQL.png";
-import msql from "../components/images/MySQL.png"
-import react from "../components/images/react.png";
-import redux from "../components/images/redux.png";
-import github from "../components/images/github.png";
-import typescript from "../components/images/TypeScript.png";
-import tailwind from "../components/images/Tailwind-CSS.png";
-import antd from "../components/images/Ant-Design.png";
-import next from "../components/images/Nextjs.png";
-import jest from "../components/images/Jest.png"
-import rtl from "../components/images/react-testing-library.png"
-import glab from "../components/images/GitLab.png"
-import jira from "../components/images/Jira.png"
-import postman from "../components/images/Postman.png"
-import docker from "../components/images/Docker.png"
+import {
+  jsVisualizerPreview, html, css, js, node, express, npm, mongo, psql, msql, react, redux, github, typescript, tailwind, antd, next, jest, rtl, glab, jira, postman, docker, copilot, chatgpt, gemini
+} from "../assets/index.js"
 
 export const portfolio = {
   name: "Smrutiranjan Patra",
@@ -290,9 +270,9 @@ export const portfolio = {
     { name: "Jira", icon: jira, group: "Workflow" },
     { name: "Postman", icon: postman, group: "Tooling" },
     { name: "npm", icon: npm, group: "Tooling" },
-    { name: "copilot", group :"AI tools"},
-    { name: "Chatgpt", group :"AI tools"},
-    { name: "Gemini", group :"AI tools"},
+    { name: "copilot", icon: copilot, group: "AI tools" },
+    { name: "Chatgpt", icon: chatgpt, group: "AI tools" },
+    { name: "Gemini", icon: gemini, group: "AI tools" },
   ],
 
   educationSection: {
